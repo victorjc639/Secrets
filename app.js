af5da788f724d16e6678452811b8f527142b6c1d
victorjc639 <victorjc639@gmail.com>
2023-07-12 14:10:22 -0700

    Connected to mongoose and updated nodemon

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const USer = new mongoose.model("User", userSchema);
 
 
 app.get("/", function(req, res){
@@ -30,3 +42,5 @@ app.get("/register", function(req, res){
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
+
+};
